39ad75458642a4d5b1a787001f4905b582c497d6
maaz malik <maaz.malik@tuhh.de>
2020-06-24 15:17:04 +0200

    Folders and lib files. for task 5

---
+++ lib/ses/ses_pwm.h
@@ -0,0 +1,32 @@
+#ifndef PWM_H_
+#define PWM_H_
+
+/*INCLUDES *******************************************************************/
+#include "ses_common.h"
+
+void pwm_init(void);
+
+void pwm_setDutyCycle(uint8_t dutyCycle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* PWM_H_ */
