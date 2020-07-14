441e6f0f5d4b46d3aae45a392346ff06779542df
ayshasundas <aysha.sundas@tuhh.de>
2020-07-14 14:05:06 +0200

    errors of static functions_6.3

--- task_6/src/Fsm.h
+++ task_6/src/Fsm.h
@@ -5,83 +5,74 @@
 #include "ses_common.h"
 
 /* TYPES ********************************************************************/
-typedef struct fsm_s Fsm; //< typedef for alarm clock state machine
-typedef struct event_s Event; //< event type for alarm clock fsm
-
+typedef struct fsm_s Fsm;        //< typedef for alarm clock state machine
+typedef struct event_s Event;    //< event type for alarm clock fsm
+typedef uint8_t fsmReturnStatus; //< typedef to be used with above enum
 /* a state is represented by a function pointer, called for
 * each transition emanating in this state */
 typedef fsmReturnStatus (*State)(Fsm *, const Event *);
 /* base type for state machine */
 
 /** return values */
-enum {
-RET_HANDLED, //< event was handled
-RET_IGNORED, //< event was ignored; not used in this implementation
-RET_TRANSITION //< event was handled and a state transition occurred
+enum
+{
+    RET_HANDLED,   //< event was handled
+    RET_IGNORED,   //< event was ignored; not used in this implementation
+    RET_TRANSITION //< event was handled and a state transition occurred
 };
 
 /** Signals */
-enum {
-ENTRY,
-EXIT,
-JOYSTICK_PRESSED,
-ROTARY_PRESSED
+enum
+{
+    ENTRY,
+    EXIT,
+    JOYSTICK_PRESSED,
+    ROTARY_PRESSED
 };
 
-
-typedef uint8_t fsmReturnStatus; //< typedef to be used with above enum
-
-typedef struct time_t {
-uint8_t hour;
-uint8_t minute;
-uint8_t second;
-uint16_t milli;
+typedef struct time_t
+{
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+    uint16_t milli;
 } time_t;
 
-struct fsm_s {
-State state; //< current state, pointer to event handler
-bool isAlarmEnabled; //< flag for the alarm status
-time_t timeSet; //< multi-purpose var for system time and alarm time
+struct fsm_s
+{
+    State state;         //< current state, pointer to event handler
+    bool isAlarmEnabled; //< flag for the alarm status
+    time_t timeSet;      //< multi-purpose var for system time and alarm time
 };
 
 /* base type for events*/
-struct event_s {
-uint8_t signal; //< identifies the type of event
+struct event_s
+{
+    uint8_t signal; //< identifies the type of event
 };
-
 
 /* dispatches events to state machine, called in application*/
 
-inline static void fsm_dispatch(Fsm * fsm, const Event * event) {
-static Event entryEvent = {.signal = ENTRY};
-static Event exitEvent = {.signal = EXIT};
-State s = fsm->state;
-fsmReturnStatus r = fsm->state(fsm, event);
-if (r == RET_TRANSITION) {
-s(fsm, &exitEvent); //< call exit action of last state
-fsm->state(fsm, &entryEvent); //< call entry action of new state
-}
+inline void fsm_dispatch(Fsm *fsm, const Event *event)
+{
 
+    Event entryEvent = {.signal = ENTRY};
+    Event exitEvent = {.signal = EXIT};
+    State s = fsm->state;
+    fsmReturnStatus r = fsm->state(fsm, event);
+    if (r == RET_TRANSITION)
+    {
+        s(fsm, &exitEvent);           //< call exit action of last state
+        fsm->state(fsm, &entryEvent); //< call entry action of new state
+    }
 
-/* sets and calls initial state of state machine */
-inline static void fsm_init(Fsm * fsm, State init) {
-//... other initialization
-Event entryEvent = {.signal = ENTRY};
-fsm->state = init;
-fsm->state(fsm, &entryEvent);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /* sets and calls initial state of state machine */
+    inline void fsm_init(Fsm * fsm, State init)
+    {
+        //... other initialization
+        Event entryEvent = {.signal = ENTRY};
+        fsm->state = init;
+        fsm->state(fsm, &entryEvent);
+    }
 
 #endif /* FSM_H_ */
--- task_6/src/main.c
+++ task_6/src/main.c
@@ -13,7 +13,9 @@
 
 #define TRANSITION(newState) (fsm->state = newState, RET_TRANSITION)
 
-time_t t1;
+
+taskDescriptor td1, td2, td3;
+Fsm clock;
 
 void Milli_to_Time(systemTime_t cT, time_t t)
 {
@@ -28,38 +30,14 @@ systemTime_t Time_to_Milli(time_t t)
     return ((t.hour * Hour_Conversion) + (t.minute * Min_Conversion));
 }
 
-fsmReturnStatus clock_init(Fsm *fsm, const Event *event)
+fsmReturnStatus normal_mode(Fsm *fsm, const Event *event)
 {
     switch (event->signal)
     {
     case ENTRY:
-        fprintf(lcdout, "00:00\nSet Time:\n");
-        return RET_HANDLED;
-    case ROTARY_PRESSED:
-        return TRANSITION(set_hours);
-    default:
-        return RET_IGNORED;
-    }
-}
-
-fsmReturnStatus set_hours(Fsm *fsm, const Event *event)
-{
-    static uint8_t hh = 0;
-    switch (event->signal)
-    {
-    case ENTRY:
+        Milli_to_Time(scheduler_getTime(), fsm->timeSet);
         lcd_clear();
-        fprintf(lcdout, "Setting Hrs:\n");
-        return RET_HANDLED;
-    case ROTARY_PRESSED:
-        hh++;
-        lcd_clear();
-        fprintf(lcdout, "%d:00\n",hh);
-        return RET_HANDLED;
-    case JOYSTICK_PRESSED:
-        return TRANSITION(set_minutes);
-    case EXIT:
-        fsm->timeSet.hour=hh;
+        fprintf(lcdout, "%d:%d:%d\n", fsm->timeSet.hour, fsm->timeSet.minute, fsm->timeSet.second);
         return RET_HANDLED;
     default:
         return RET_IGNORED;
@@ -78,13 +56,13 @@ fsmReturnStatus set_minutes(Fsm *fsm, const Event *event)
     case ROTARY_PRESSED:
         mm++;
         lcd_clear();
-        fprintf(lcdout, "%d:%d\n",fsm->timeSet.hour,mm);
+        fprintf(lcdout, "%d:%d\n", fsm->timeSet.hour, mm);
         return RET_HANDLED;
     case JOYSTICK_PRESSED:
         return TRANSITION(normal_mode);
     case EXIT:
-        fsm->timeSet.minute=mm;
-        scheduler_setTime(Time_to_Milli(fsm->timeSet));//Updating System time
+        fsm->timeSet.minute = mm;
+        scheduler_setTime(Time_to_Milli(fsm->timeSet)); //Updating System time
         //starts the clock???????
         return RET_HANDLED;
     default:
@@ -92,12 +70,108 @@ fsmReturnStatus set_minutes(Fsm *fsm, const Event *event)
     }
 }
 
-fsmReturnStatus normal_mode(Fsm *fsm, const Event *event)
-{
 
+fsmReturnStatus set_hours(Fsm *fsm, const Event *event)
+{
+    static uint8_t hh = 0;
+    switch (event->signal)
+    {
+    case ENTRY:
+        lcd_clear();
+        fprintf(lcdout, "Setting Hrs:\n");
+        return RET_HANDLED;
+    case ROTARY_PRESSED:
+        hh++;
+        lcd_clear();
+        fprintf(lcdout, "%d:00\n", hh);
+        return RET_HANDLED;
+    case JOYSTICK_PRESSED:
+        return TRANSITION(set_minutes);
+    case EXIT:
+        fsm->timeSet.hour = hh;
+        return RET_HANDLED;
+    default:
+        return RET_IGNORED;
+    }
 }
 
-int main()
+fsmReturnStatus clock_init(Fsm *fsm, const Event *event)
 {
+    switch (event->signal)
+    {
+    case ENTRY:
+        fprintf(lcdout, "00:00\nSet Time:\n");
+        return RET_HANDLED;
+    case ROTARY_PRESSED:
+        return TRANSITION(set_hours);
+    default:
+        return RET_IGNORED;
+    }
+}
+
+
+
+
+void joystickPressedDispatch(void *param)
+{
+    Event e = {.signal = JOYSTICK_PRESSED};
+    fsm_dispatch(&clock, &e);
+}
+
+void rotaryPressedDispatch(void *param)
+{
+    Event e = {.signal = ROTARY_PRESSED};
+    fsm_dispatch(&clock, &e);
+}
+
+void button_debouncing(void *ptr)
+{ //Task for debouncing the buttons and calling their callbacks when a particular button is pressed, respectively
+
+    button_checkState();
+}
+
+void callback_for_joystick()
+{
+    scheduler_add(&td3);
+}
+
+void callback_for_rotary()
+{
+    scheduler_add(&td2);
+}
+
+int main(void)
+{
+
     lcd_init();
+    button_setJoystickButtonCallback(callback_for_joystick);
+    button_setRotaryButtonCallback(callback_for_rotary);
+    led_greenInit();
+    led_redInit();
+    led_yellowInit();
+    button_init(1);
+
+    td1.period = 5;
+    td1.expire = td1.period;
+    td1.param = NULL;
+    td1.task = button_debouncing;
+
+    td2.period = 0;
+    td2.expire = 0;
+    td2.execute = 1;
+    td2.param = NULL;
+    td2.task = rotaryPressedDispatch;
+
+    td3.period = 0;
+    td3.expire = 0;
+    td3.execute = 1;
+    td3.param = NULL;
+    td3.task = joystickPressedDispatch;
+
+    scheduler_add(&td1);
+
+    scheduler_init();
+    sei();
+    fsm_init(&clock, clock_init);
+    scheduler_run();
 }
