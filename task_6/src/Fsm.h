af4d1e4f2bd29591155a73b95d5ac7191a6c8c88
ayshasundas <aysha.sundas@tuhh.de>
2020-07-14 02:13:03 +0200

    6.3 started

--- task_6/src/Fsm.h
+++ task_6/src/Fsm.h
@@ -5,32 +5,70 @@
 #include "ses_common.h"
 
 /* TYPES ********************************************************************/
-typedef struct Event Event;
-typedef struct Fsm Fsm;
+typedef struct fsm_s Fsm; //< typedef for alarm clock state machine
+typedef struct event_s Event; //< event type for alarm clock fsm
 
 /* a state is represented by a function pointer, called for
 * each transition emanating in this state */
-typedef void (*State)(Fsm *, const Event *);
+typedef fsmReturnStatus (*State)(Fsm *, const Event *);
 /* base type for state machine */
 
-struct Fsm {
-State state; /* current state */
+/** return values */
+enum {
+RET_HANDLED, //< event was handled
+RET_IGNORED, //< event was ignored; not used in this implementation
+RET_TRANSITION //< event was handled and a state transition occurred
+};
+
+/** Signals */
+enum {
+ENTRY,
+EXIT,
+JOYSTICK_PRESSED,
+ROTARY_PRESSED
+};
+
+
+typedef uint8_t fsmReturnStatus; //< typedef to be used with above enum
+
+typedef struct time_t {
+uint8_t hour;
+uint8_t minute;
+uint8_t second;
+uint16_t milli;
+} time_t;
+
+struct fsm_s {
+State state; //< current state, pointer to event handler
+bool isAlarmEnabled; //< flag for the alarm status
+time_t timeSet; //< multi-purpose var for system time and alarm time
 };
 
 /* base type for events*/
-struct Event {
-int signal;
+struct event_s {
+uint8_t signal; //< identifies the type of event
 };
 
+
 /* dispatches events to state machine, called in application*/
+
 inline static void fsm_dispatch(Fsm * fsm, const Event * event) {
-fsm->state(fsm, event);
+static Event entryEvent = {.signal = ENTRY};
+static Event exitEvent = {.signal = EXIT};
+State s = fsm->state;
+fsmReturnStatus r = fsm->state(fsm, event);
+if (r == RET_TRANSITION) {
+s(fsm, &exitEvent); //< call exit action of last state
+fsm->state(fsm, &entryEvent); //< call entry action of new state
 }
+
 
 /* sets and calls initial state of state machine */
 inline static void fsm_init(Fsm * fsm, State init) {
+//... other initialization
+Event entryEvent = {.signal = ENTRY};
 fsm->state = init;
-fsm_dispatch(fsm, NULL);
+fsm->state(fsm, &entryEvent);
 }
 
 
--- task_6/src/main.c
+++ task_6/src/main.c
@@ -7,40 +7,97 @@
 #include "ses_button.h"
 #include "Fsm.h"
 
+#define Hour_Conversion 3600000
+#define Min_Conversion 60000
+#define Sec_Conversion 1000
 
-
-#define Hour_Conversion     3600000
-#define Min_Conversion     60000
-#define Sec_Conversion     1000
-
-typedef struct time_t {
-uint8_t hour;
-uint8_t minute;
-uint8_t second;
-uint16_t milli;
-} time_t;
+#define TRANSITION(newState) (fsm->state = newState, RET_TRANSITION)
 
 time_t t1;
 
 void Milli_to_Time(systemTime_t cT, time_t t)
 {
-    t.hour= (cT/Hour_Conversion);
-    t.minute= ((cT-(t.hour*Hour_Conversion))/Min_Conversion);
-    t.second= ((cT-(t.hour*Hour_Conversion)-(t.minute*Min_Conversion))/Sec_Conversion);
-    t.milli= ((cT-(t.hour*Hour_Conversion)-(t.minute*Min_Conversion)-(t.second*Sec_Conversion))/Min_Conversion);
-
+    t.hour = (cT / Hour_Conversion);
+    t.minute = ((cT - (t.hour * Hour_Conversion)) / Min_Conversion);
+    t.second = ((cT - (t.hour * Hour_Conversion) - (t.minute * Min_Conversion)) / Sec_Conversion);
+    t.milli = ((cT - (t.hour * Hour_Conversion) - (t.minute * Min_Conversion) - (t.second * Sec_Conversion)) / Min_Conversion);
 }
 
 systemTime_t Time_to_Milli(time_t t)
 {
-   return ( (t.hour*Hour_Conversion) + (t.minute*Min_Conversion) );
+    return ((t.hour * Hour_Conversion) + (t.minute * Min_Conversion));
+}
+
+fsmReturnStatus clock_init(Fsm *fsm, const Event *event)
+{
+    switch (event->signal)
+    {
+    case ENTRY:
+        fprintf(lcdout, "00:00\nSet Time:\n");
+        return RET_HANDLED;
+    case ROTARY_PRESSED:
+        return TRANSITION(set_hours);
+    default:
+        return RET_IGNORED;
+    }
+}
+
+fsmReturnStatus set_hours(Fsm *fsm, const Event *event)
+{
+    static uint8_t hh = 0;
+    switch (event->signal)
+    {
+    case ENTRY:
+        lcd_clear();
+        fprintf(lcdout, "Setting Hrs:\n");
+        return RET_HANDLED;
+    case ROTARY_PRESSED:
+        hh++;
+        lcd_clear();
+        fprintf(lcdout, "%d:00\n",hh);
+        return RET_HANDLED;
+    case JOYSTICK_PRESSED:
+        return TRANSITION(set_minutes);
+    case EXIT:
+        fsm->timeSet.hour=hh;
+        return RET_HANDLED;
+    default:
+        return RET_IGNORED;
+    }
+}
+
+fsmReturnStatus set_minutes(Fsm *fsm, const Event *event)
+{
+    static uint8_t mm = 0;
+    switch (event->signal)
+    {
+    case ENTRY:
+        lcd_clear();
+        fprintf(lcdout, "Setting Min:\n");
+        return RET_HANDLED;
+    case ROTARY_PRESSED:
+        mm++;
+        lcd_clear();
+        fprintf(lcdout, "%d:%d\n",fsm->timeSet.hour,mm);
+        return RET_HANDLED;
+    case JOYSTICK_PRESSED:
+        return TRANSITION(normal_mode);
+    case EXIT:
+        fsm->timeSet.minute=mm;
+        scheduler_setTime(Time_to_Milli(fsm->timeSet));//Updating System time
+        //starts the clock???????
+        return RET_HANDLED;
+    default:
+        return RET_IGNORED;
+    }
+}
+
+fsmReturnStatus normal_mode(Fsm *fsm, const Event *event)
+{
 
 }
 
-
-
 int main()
 {
-
-    
+    lcd_init();
 }
