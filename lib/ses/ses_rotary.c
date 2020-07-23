34f481e12ec9c3d1d018d9cda53513f7cd411b41
maaz malik <maaz.malik@tuhh.de>
2020-07-23 15:53:36 +0200

    final version task 6

--- lib/ses/ses_rotary.c
+++ lib/ses/ses_rotary.c
@@ -13,21 +13,17 @@
 #define B_ROTARY_PORT PORTG //PG2
 #define B_ROTARY_PIN 2
 
-#define BUTTON_NUM_DEBOUNCE_CHECKS 5
+#define ROTARY_NUM_DETENT_CHECKS 5
+#define Clockwise              0x01
+#define Anti_Clockwise         0x02
 
-static volatile int counter = 0;
 static volatile int flag = 0;
 
-//#define BUTTON_NUM_DEBOUNCE_CHECKS      5 // Number of times a button bounces, after which we consider it as a single button press
-//#define BUTTON_DEBOUNCE_POS_JOYSTICK    0x01
-//#define BUTTON_DEBOUNCE_POS_ROTARY      0x02
-
-static volatile pTypeRotaryCallback a; // global volatile function pointer for joystick button callback
-static volatile pTypeRotaryCallback b; // global volatile function pointer for rotary button callback
+static volatile pTypeRotaryCallback a; // global volatile function pointer for Clockwise Rotation callback
+static volatile pTypeRotaryCallback b; // global volatile function pointer for AntiClockwise Rotation callback
 
 void rotary_setClockwiseCallback(pTypeRotaryCallback callback)
 {
-    //callback for Right button
     if (callback)
     {
         a = callback;
@@ -36,7 +32,6 @@ void rotary_setClockwiseCallback(pTypeRotaryCallback callback)
 
 void rotary_setCounterClockwiseCallback(pTypeRotaryCallback callback)
 {
-    //callback for rotary button
     if (callback)
     {
         b = callback;
@@ -44,7 +39,7 @@ void rotary_setCounterClockwiseCallback(pTypeRotaryCallback callback)
 }
 
 bool button_isAPressed(void)
-{ // For getting the state of the joystick button.
+{ 
     if (!(PIN_REGISTER(A_ROTARY_PORT) & (1 << A_ROTARY_PIN)))
     {
         return 1;
@@ -56,7 +51,7 @@ bool button_isAPressed(void)
 }
 
 bool button_isBPressed(void)
-{ // For getting the state of the rotary button.
+{ 
     if (!(PIN_REGISTER(B_ROTARY_PORT) & (1 << B_ROTARY_PIN)))
     {
         return 1;
@@ -70,54 +65,57 @@ bool button_isBPressed(void)
 void check_rotary()
 {
     uart_init(57600);
-    static uint8_t state[BUTTON_NUM_DEBOUNCE_CHECKS] = {};
+    static uint8_t state[ROTARY_NUM_DETENT_CHECKS] = {};
     static uint8_t index = 0;
-    static uint8_t debouncedState = 0;
-    uint8_t lastDebouncedState = debouncedState;
+    static uint8_t CurrentState = 0;
+    uint8_t lastState = CurrentState;
     state[index] = 0;
     if (button_isAPressed())
     {
-        state[index] |= 0x01;
+        state[index] |= Clockwise;
     }
     if (button_isBPressed())
     {
-        state[index] |= 0x02;
+        state[index] |= Anti_Clockwise;
         
     }
 
     index++;
 
-    if (index == BUTTON_NUM_DEBOUNCE_CHECKS)
+    if (index == ROTARY_NUM_DETENT_CHECKS)
     {
         index = 0;
     }
     
+
     uint8_t j = 0xFF;
-    for (uint8_t i = 0; i < BUTTON_NUM_DEBOUNCE_CHECKS; i++)
+    for (uint8_t i = 0; i < ROTARY_NUM_DETENT_CHECKS; i++)
     {
         j = j & state[i];
         
     }
     
-    debouncedState = j;
+    CurrentState = j;
 
     
-
-    if ((lastDebouncedState != debouncedState) & (lastDebouncedState == 0x01) & (flag == 0))
+    /*
+    **Since both signals arives while using rotary encoder, following code 
+    **checks the change in states and decides on the last state because we are  
+    **only interested in whatever input is on first in order to decide whether
+    **Rotary is rotated Clockwise or AntiClockwise
+    */
+    if ((lastState != CurrentState) && (lastState == Clockwise) && (flag == 0))
     {
-
         flag++;
-        fprintf(uartout, "Clockwise and right is pressed\n");
         a();
     }
 
-    else if ((lastDebouncedState != debouncedState) & (lastDebouncedState == 0x02) & (flag == 0))
+    else if ((lastState != CurrentState) && (lastState == Anti_Clockwise) && (flag == 0))
     {
         flag++;
-        fprintf(uartout, "AntiClockwise and left is pressed\n");
         b();
     }
-    else if ((lastDebouncedState != debouncedState) & !(lastDebouncedState == 0x03))
+    else if ((lastState != CurrentState) && (lastState == Clockwise || lastState == Anti_Clockwise))
     {
         flag = 0;
     }
--- lib/ses/ses_rotary.h
+++ lib/ses/ses_rotary.h
@@ -11,33 +11,36 @@ typedef void (*pTypeRotaryCallback)();//defining function pointer type
 /* FUNCTION PROTOTYPES *******************************************************/
 
 /**
- * Initializes rotary encoder and joystick button
- * @param debouncing    flag that specifies if debouncing or direct interrrupt technique should be used 
+ * Initializes rotary encoder 
  */
 void rotary_init();
 
 /** 
- * Get the state of the joystick button.
+ * Get the state of the Rotary Input A.
+ * @return a bool indicating either Rotary Input A is on or off
  */
 bool button_isAPressed(void);
 
 /** 
- * Get the state of the rotary button.
+ * Get the state of the Rotary Input B.
+ * @return a bool indicating either Rotary Input B is on or off
  */
+
 bool button_isBPressed(void);
+
 /**
- * For debouncing the buttons
+ * For handling mechanical detents
  */ 
 void check_rotary();
 /**
- *  Sets a function to be called when rotary button is pressed
+ *  Sets a function to be called when Rotary is rotated clockwise
  *
  * @param callback  pointer to the callback function; if NULL, no callback
  *                  will be executed.
  */ 
 void rotary_setClockwiseCallback(pTypeRotaryCallback callback);
 /**
- *  Sets a function to be called when joystick button is pressed
+ *  Sets a function to be called when Rotary is rotated Anticlockwise
  *
  * @param callback  pointer to the callback function; if NULL, no callback
  *                  will be executed.
